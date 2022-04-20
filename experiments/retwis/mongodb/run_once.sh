16674a46eea074d81c7e21e5904838f6ac30216f
kevin george <kgeorge38@gatech.edu>
2022-04-19 22:43:11 -0400

    book keeping

--- experiments/retwis/mongodb/run_once.sh
+++ experiments/retwis/mongodb/run_once.sh
@@ -14,6 +14,8 @@ CLIENT_HOST=`$HELPER_SCRIPT get-client-host --base-dir=$BASE_DIR`
 ENTRY_HOST=`$HELPER_SCRIPT get-service-host --base-dir=$BASE_DIR --service=boki-gateway`
 ALL_HOSTS=`$HELPER_SCRIPT get-all-server-hosts --base-dir=$BASE_DIR`
 
+echo $MANAGER_HOST
+
 $HELPER_SCRIPT generate-docker-compose --base-dir=$BASE_DIR
 scp -q $BASE_DIR/docker-compose.yml $MANAGER_HOST:~
 scp -q $BASE_DIR/docker-compose-generated.yml $MANAGER_HOST:~
@@ -29,7 +31,7 @@ scp -q $BASE_DIR/init_mongo_rs.js $MANAGER_HOST:/tmp/init_mongo_rs.js
 for host in $ALL_HOSTS; do
     scp -q $BASE_DIR/nightcore_config.json $host:/tmp/nightcore_config.json
 done
-
+echo "1"
 ALL_ENGINE_HOSTS=`$HELPER_SCRIPT get-machine-with-label --base-dir=$BASE_DIR --machine-label=engine_node`
 for HOST in $ALL_ENGINE_HOSTS; do
     scp -q $BASE_DIR/run_launcher $HOST:/tmp/run_launcher
@@ -39,24 +41,23 @@ for HOST in $ALL_ENGINE_HOSTS; do
     ssh -q $HOST -- sudo cp /tmp/run_launcher /mnt/inmem/boki/run_launcher
     ssh -q $HOST -- sudo cp /tmp/nightcore_config.json /mnt/inmem/boki/func_config.json
 done
-
+echo "after 1"
 ALL_STORAGE_HOSTS=`$HELPER_SCRIPT get-machine-with-label --base-dir=$BASE_DIR --machine-label=storage_node`
 for HOST in $ALL_STORAGE_HOSTS; do
     ssh -q $HOST -- sudo rm -rf   /mnt/storage/mongo
     ssh -q $HOST -- sudo mkdir -p /mnt/storage/mongo
 done
-
-ssh -q $MANAGER_HOST -- docker stack deploy \
-    -c ~/docker-compose-generated.yml -c ~/docker-compose.yml boki-experiment
+echo "2"
+sudo ssh -q $MANAGER_HOST -- 'docker stack deploy -c ~/docker-compose-generated.yml -c ~/docker-compose.yml boki-experiment'
 sleep 60
-
+echo "after 2"
 for HOST in $ALL_ENGINE_HOSTS; do
     ENGINE_CONTAINER_ID=`$HELPER_SCRIPT get-container-id --base-dir=$BASE_DIR --service faas-engine --machine-host $HOST`
     echo 4096 | ssh -q $HOST -- sudo tee /sys/fs/cgroup/cpu,cpuacct/docker/$ENGINE_CONTAINER_ID/cpu.shares
 done
 
 sleep 10
-
+echo "done somethni"
 rm -rf $EXP_DIR
 mkdir -p $EXP_DIR
 
@@ -66,14 +67,14 @@ ssh -q $MANAGER_HOST -- uname -a >>$EXP_DIR/kernel_version
 ssh -q $CLIENT_HOST -- curl -X POST http://$ENTRY_HOST:8080/function/mongoRetwisInit
 
 ssh -q $CLIENT_HOST -- docker run -v /tmp:/tmp \
-    kevinboki/boki-retwisbench:sosp-ae \
+    zjia/boki-retwisbench:sosp-ae \
     cp /retwisbench-bin/create_users /tmp/create_users
 
 ssh -q $CLIENT_HOST -- /tmp/create_users \
     --faas_gateway=$ENTRY_HOST:8080 --fn_prefix=mongo --num_users=$NUM_USERS --concurrency=24
 
 ssh -q $CLIENT_HOST -- docker run -v /tmp:/tmp \
-    kevinboki/boki-retwisbench:sosp-ae \
+    zjia/boki-retwisbench:sosp-ae \
     cp /retwisbench-bin/benchmark /tmp/benchmark
 
 ssh -q $CLIENT_HOST -- /tmp/benchmark \
